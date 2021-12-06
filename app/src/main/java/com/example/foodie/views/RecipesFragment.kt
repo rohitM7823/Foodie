768ffb58a698507ea92bd1ca305a6720ef1adbb5
Rohit Manna <rohitmanna9674@gmail.com>
2021-12-06 09:48:55 +0530

    Foodie Unfinished

--- app/src/main/java/com/example/foodie/views/RecipesFragment.kt
+++ app/src/main/java/com/example/foodie/views/RecipesFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
